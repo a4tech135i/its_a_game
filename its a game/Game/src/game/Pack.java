b51c68cbc6a8c6ada73ceff2f1df61944aaf6795
SaNeKxOK <kuchmiy8@gmail.com>
2018-03-10 19:26:12 +0200

    + getByValue/getByTheme/getByThemeAndValue

--- its a game/Game/src/game/Pack.java
+++ its a game/Game/src/game/Pack.java
@@ -13,38 +13,74 @@ import java.util.Random;
  *
  * @author admin
  */
-public class Pack implements Serializable{
+public class Pack implements Serializable {
+
     private String Name;
     private ArrayList<Question> Questions;
-    
-    //Конструктор
 
     public Pack(String Name, ArrayList<Question> Questions) {
         this.Name = Name;
         this.Questions = Questions;
     }
 
-    
-    //Конструктор для побудови теста на основі існуючого
-    public Pack(int n, Pack test)
-    {
-        Name=test.getName();
+    public Question getQuestionByThemeAndValue(String theme, int value) {
         Questions = new ArrayList<>();
-        
-        ArrayList<Question> tmp=test.getQuestions();
-        ArrayList<Integer> usedIndex=new ArrayList<>();
-        
-        for(int i=0;i<n;i++)
-        {
+
+        ArrayList<Question> tmp = getQuestionByValue(getQuestionByTheme(theme), value);
+        Random randob = new Random();
+        return tmp.get(randob.nextInt(tmp.size()));
+    }
+
+    public ArrayList<Question> getQuestionByTheme(String theme) {
+        Questions = new ArrayList<>();
+
+        ArrayList<Question> tmp = new ArrayList<Question>();
+
+        for (int i = 0; i < Questions.size(); i++) {
+            if (Questions.get(i).getTheme() == theme) {
+                tmp.add(Questions.get(i));
+            }
+        }
+        return tmp;
+    }
+
+    public ArrayList<Question> getQuestionByValue(ArrayList<Question> q, int val) {
+        Questions = new ArrayList<>();
+
+        ArrayList<Question> tmp = new ArrayList<Question>();
+
+        for (int i = 0; i < q.size(); i++) {
+            if (q.get(i).getValue()== val) {
+                tmp.add(q.get(i));
+            }
+        }
+        return tmp;
+    }
+
+    public Pack(int n, Pack test) {
+        Name = test.getName();
+        Questions = new ArrayList<>();
+
+        ArrayList<Question> tmp = test.getQuestions();
+        ArrayList<Integer> usedIndex = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             Random randob = new Random();
-            int index=randob.nextInt(tmp.size());
-            while(usedIndex.contains(index)) index=randob.nextInt(tmp.size());
+            int index = randob.nextInt(tmp.size());
+            while (usedIndex.contains(index)) {
+                index = randob.nextInt(tmp.size());
+            }
             usedIndex.add(index);
             Questions.add(tmp.get(index));
         }
     }
-    
-    public ArrayList<Question> getQuestions(){return Questions;}
-    public String getName(){return Name;}
-    
+
+    public ArrayList<Question> getQuestions() {
+        return Questions;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
 }
